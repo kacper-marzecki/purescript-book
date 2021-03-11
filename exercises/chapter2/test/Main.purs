1c4fa3a1001de392b1bd350edc4fc3388e9b2723
Kacper Marzecki <60017757+kacper-marzecki@users.noreply.github.com>
2021-03-11 23:55:42 +0100

    chapter 2 completed

--- exercises/chapter2/test/Main.purs
+++ exercises/chapter2/test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 import Test.MySolutions
-import Test.NoPeeking.Solutions  -- Note to reader: Delete this line
+-- import Test.NoPeeking.Solutions  -- Note to reader: Delete this line
 import Effect (Effect)
 import Euler (answer)
 import Test.Unit (suite, test)
